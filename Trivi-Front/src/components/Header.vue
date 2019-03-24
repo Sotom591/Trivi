8e8875200437335d5ff72e18d1cc2377a82485bf
github username <github email address>
2019-03-24 17:03:16 -0400

    skeleton and first fetch

---
+++ Trivi-Front/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Trivi</b>
+      </b-nav-item>
+      <b-nav-item disabled>Correct: 2/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ Trivi-Front/src/components/QuestionBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="question-box-cont">
+    <b-jumbotron>
+      <template slot="lead">{{currentQu.question}}</template>
+
+      <hr class="my-4">
+
+      <p>Answers Here</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQu: Object
+  }
+}
+</script>
